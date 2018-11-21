a284b128ec8dee13f20ad8168d030373374dab63
Fabian Peter Hammerle <fabian@hammerle.me>
2018-11-21 22:16:42 +0100

    setup.py: added url to repo on github

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setuptools.setup(
     use_scm_version=True,
     author='Fabian Peter Hammerle',
     author_email='fabian@hammerle.me',
+    url='https://github.com/fphammerle/acpi-backlight',
     packages=['acpi_backlight'],
     setup_requires=['setuptools_scm'],
     classifiers=[
